79560509ee11c65e6e99634675d0c6dcac956b65
Gihun Ham <progh2@gmail.com>
2020-06-16 10:33:10 +0900

    초기 커밋.

---
+++ SealedPrac/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SealedPrac
+{
+    class Program
+    {
+        class Parent
+        {
+            public virtual void Method() { }
+        }
+
+        class Child : Parent
+        {
+            public sealed override void Method() { }
+        }
+
+        class GrandChild : Child
+        {
+            //public override void Method(){ }
+        }
+
+        abstract class Parent2
+        {
+            public abstract void Method();
+        }
+
+        class child2 : Parent2
+        {
+            public override void Method()
+            {
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
